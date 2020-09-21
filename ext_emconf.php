b194c5cb37ca49ca156c02b6170dde5707806aca
Remo Schneider <remo.schneider@gmx.de>
2020-09-21 10:42:08 +0200

    Update declaration file
    * Replace array() with short syntax
    * Raise TYPO3 version to 10.4.6
    * Raise self version to 1.4.0
    * Remove deprecated entries

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,46 +10,25 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'FAL FTP Driver',
 	'description' => 'Provides a FTP and FTP-SSL driver for the TYPO3 File Abstraction Layer (FAL) to manage files via filemanager (filelist).',
 	'category' => 'plugin',
-	'shy' => 0,
-	'version' => '1.3.0',
-	'dependencies' => 'cms,version',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
+	'version' => '1.4.0',
 	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
 	'clearcacheonload' => 1,
-	'lockType' => '',
 	'author' => 'Arno Dudek',
 	'author_email' => 'webmaster@adgrafik.at',
 	'author_company' => 'ad:grafik',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'cms' => '',
-			'version' => '',
-			'typo3' => '7.4.0-7.6.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-	'suggests' => 
-	array (
-	),
-);
+	'constraints' =>  [
+		'depends' =>  [
+			'typo3' => '10.4.6-10.4.99',
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+];
 
 ?>
